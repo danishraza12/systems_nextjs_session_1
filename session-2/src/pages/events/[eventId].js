911401aaa97bf5af0b71cec1867effd14a78f49c
danishraza_12 <danish.raza@systemsltd.com>
2023-01-30 22:16:58 +0500

    add: Course Section 4 - resource 81

--- session-2/src/pages/events/[eventId].js
+++ session-2/src/pages/events/[eventId].js
@@ -1,12 +1,30 @@
-import useRouter from "next/router"
+import { getEventById } from "dummy-data";
+import { useRouter } from "next/router"
+import { Fragment } from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
 
 export default function EventDetailPage () {
-    // const router = useRouter();
-    // const id = router.query;
+    const router = useRouter();
+    const eventId = router.query.eventId;
+    console.log("EVENT ID: ", router.query)
+    const event = getEventById(eventId);
+    
+    if (!event) {
+        return (
+            <p>No event found!</p>
+        )
+    }
+    const { title, date, location, image, description } = event;
 
     return (
-        <div>
-            <h1>Event</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={title} />
+            <EventLogistics date={date} address={location} image={image} imageAlt={title} />
+            <EventContent>
+                <p>{description}</p>
+            </EventContent>
+        </Fragment>
     )
 }
